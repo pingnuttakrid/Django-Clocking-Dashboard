6a5951a779e71687916b949d8c278aeb2b6d26a7
Nuttakrid Uppatumwipanon <pingnuttakrid@gmail.com>
2020-07-27 21:24:51 +0700

    edit

--- faceweb/forms.py
+++ faceweb/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class EmployeeForm(forms.ModelForm):
     class Meta:
         model = Employee
-        fields = ('firstname','lastname','email','status','Type')
+        fields = ('title','firstname','lastname','email','status','Type','contact','adresss','adcontact','overseaadresss','ovcontact','emerseaadresss','emercontact')
         
 class ImageForm(forms.ModelForm):
     class Meta:
